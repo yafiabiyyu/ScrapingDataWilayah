5d5796b4e945e690518fd6a0e923f4951a0368ad
Abiyyu Yafi <yafiabiyyu@protonmail.com>
2022-03-23 22:22:35 +0700

    fix getDes

--- DataWilayahScrap.py
+++ DataWilayahScrap.py
@@ -4,6 +4,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+
 class DataWilayahScrap:
     def __init__(self):
         self.provinsi = []
@@ -107,14 +108,18 @@ class DataWilayah:
             "https://sig.bps.go.id/rest-bridging/getwilayah?level=provinsi"
         ).json()
         for i in range(len(reqProv)):
-            print("Ambil Data Provinsi {}".format(reqProv[i]["nama_bps"]))
-            self.provinsi["kode_bps"].append(str(reqProv[i]["kode_bps"]))
+            print(
+                "Ambil Data Provinsi {}".format(reqProv[i]["nama_bps"])
+            )
+            self.provinsi["kode_bps"].append(
+                str(reqProv[i]["kode_bps"])
+            )
             self.provinsi["nama_bps"].append(reqProv[i]["nama_bps"])
             self.provinsi["kode_dagri"].append(reqProv[i]["kode_dagri"])
             self.provinsi["nama_dagri"].append(reqProv[i]["nama_dagri"])
-    
+
     def getKab(self):
-        self.kabupaten['kode_provinsi'] = []
+        self.kabupaten["kode_provinsi"] = []
         self.kabupaten["kode_bps"] = []
         self.kabupaten["nama_bps"] = []
         self.kabupaten["kode_dagri"] = []
@@ -126,15 +131,27 @@ class DataWilayah:
                 )
             ).json()
             for j in range(len(reqKab)):
-                print("Ambil Data Kabupaten {}".format(reqKab[j]["nama_bps"]))
-                self.kabupaten["kode_provinsi"].append(str(self.provinsi["kode_bps"][i]))
-                self.kabupaten["kode_bps"].append(str(reqKab[j]["kode_bps"]))
+                print(
+                    "Ambil Data Kabupaten {}".format(
+                        reqKab[j]["nama_bps"]
+                    )
+                )
+                self.kabupaten["kode_provinsi"].append(
+                    str(self.provinsi["kode_bps"][i])
+                )
+                self.kabupaten["kode_bps"].append(
+                    str(reqKab[j]["kode_bps"])
+                )
                 self.kabupaten["nama_bps"].append(reqKab[j]["nama_bps"])
-                self.kabupaten["kode_dagri"].append(reqKab[j]["kode_dagri"])
-                self.kabupaten["nama_dagri"].append(reqKab[j]["nama_dagri"])
-    
+                self.kabupaten["kode_dagri"].append(
+                    reqKab[j]["kode_dagri"]
+                )
+                self.kabupaten["nama_dagri"].append(
+                    reqKab[j]["nama_dagri"]
+                )
+
     def getKec(self):
-        self.kecamatan['kode_kab/kota'] = []
+        self.kecamatan["kode_kab/kota"] = []
         self.kecamatan["kode_bps"] = []
         self.kecamatan["nama_bps"] = []
         self.kecamatan["kode_dagri"] = []
@@ -146,19 +163,31 @@ class DataWilayah:
                 )
             ).json()
             for j in range(len(reqKec)):
-                print("Ambil Data Kecamatan {}".format(reqKec[j]["nama_bps"]))
-                self.kecamatan["kode_kab/kota"].append(str(self.kabupaten["kode_bps"][i]))
-                self.kecamatan["kode_bps"].append(str(reqKec[j]["kode_bps"]))
+                print(
+                    "Ambil Data Kecamatan {}".format(
+                        reqKec[j]["nama_bps"]
+                    )
+                )
+                self.kecamatan["kode_kab/kota"].append(
+                    str(self.kabupaten["kode_bps"][i])
+                )
+                self.kecamatan["kode_bps"].append(
+                    str(reqKec[j]["kode_bps"])
+                )
                 self.kecamatan["nama_bps"].append(reqKec[j]["nama_bps"])
-                self.kecamatan["kode_dagri"].append(reqKec[j]["kode_dagri"])
-                self.kecamatan["nama_dagri"].append(reqKec[j]["nama_dagri"])
-    
+                self.kecamatan["kode_dagri"].append(
+                    reqKec[j]["kode_dagri"]
+                )
+                self.kecamatan["nama_dagri"].append(
+                    reqKec[j]["nama_dagri"]
+                )
+
     def getDes(self):
-        self.desa['kode_kec'] = []
+        self.desa["kode_kec"] = []
         self.desa["kode_bps"] = []
         self.desa["nama_bps"] = []
-        self.kecamatan["kode_dagri"] = []
-        self.kecamatan["nama_dagri"] = []
+        self.desa["kode_dagri"] = []
+        self.desa["nama_dagri"] = []
         for i in range(len(self.kecamatan["kode_bps"])):
             reqDes = requests.get(
                 "https://sig.bps.go.id/rest-bridging/getwilayah?level=desa&parent={}".format(
@@ -166,8 +195,12 @@ class DataWilayah:
                 )
             ).json()
             for j in range(len(reqDes)):
-                print("Ambil Data Desa {}".format(reqDes[j]["nama_bps"]))
-                self.desa["kode_kec"].append(str(self.kecamatan["kode_bps"][i]))
+                print(
+                    "Ambil Data Desa {}".format(reqDes[j]["nama_bps"])
+                )
+                self.desa["kode_kec"].append(
+                    str(self.kecamatan["kode_bps"][i])
+                )
                 self.desa["kode_bps"].append(str(reqDes[j]["kode_bps"]))
                 self.desa["nama_bps"].append(reqDes[j]["nama_bps"])
                 self.desa["kode_dagri"].append(reqDes[j]["kode_dagri"])
@@ -186,13 +219,13 @@ class DataWilayah:
         write.save()
 
     def uploadToIPFS(self):
-        w3_token = os.getenv('storage_token')
+        w3_token = os.getenv("storage_token")
         root_url = "https://api.web3.storage/upload"
-        files = {'file': open('dataWilayah.xlsx', 'rb')}
-        headers = {'Authorization': 'Bearer ' + w3_token}
+        files = {"file": open("dataWilayah.xlsx", "rb")}
+        headers = {"Authorization": "Bearer " + w3_token}
         r = requests.post(root_url, files=files, headers=headers)
         print("CID Mu nih : {}".format(r.json()["cid"]))
-    
+
     def main(self):
         self.getProv()
         self.getKab()
